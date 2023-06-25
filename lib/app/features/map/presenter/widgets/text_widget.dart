f70a3e85fae83d77ce08fcacd852f73d21bf34d4
Kevin-Sarges <kevinsarges08@gmail.com>
2023-06-25 11:39:24 -0300

    feat: novo widget da tela de map

---
+++ lib/app/features/map/presenter/widgets/text_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({
+    super.key,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          subtitle,
+          style: const TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
